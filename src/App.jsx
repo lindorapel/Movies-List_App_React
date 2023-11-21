cfc64168275f92d3c7a788e66452f617aabbc993
lindorapel <lindorapel@gmail.com>
2023-11-21 10:33:07 +0700

    edit app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import DetailMovies from "./pages/DetailMovies";
-// import PopularMovies from "./pages/home/PopularMovies.";
 import Home from "./pages/home/Home";
 import PopularMovies from "./pages/PopularMovies";
 import SearchResult from "./pages/SearchResult";
@@ -11,8 +10,8 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResult />} />
+        <Route path="/" element={<Home />} />
         <Route path="/details/:movieId" element={<DetailMovies />} />
         <Route path="/popular-movies" element={<PopularMovies />} />
       </Routes>
